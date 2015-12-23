53377d273178d335120a142b91abffa56cc42ed8
Ashley Joyce <ashleyjoyce72@gmail.com>
2015-12-24 07:09:09 +1100

    my solution to the assignment

--- cachematrix.R
+++ cachematrix.R
@@ -1,5 +1,5 @@
 ## Put comments here that give an overall description of what your
-## functions do
+## functions do.
 
 ## Write a short comment describing this function
 ## This function creates a matrix to chage its inversible. 
